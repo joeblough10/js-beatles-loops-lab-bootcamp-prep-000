7574ca09c00c0fc204c10ac52a9ac43bbed3595f
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-22 02:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function johnLennonFacts(facts) {
  var newFacts = []
  var i = 0;
  while (i < 4) {
-   facts[i] = facts[i] + "!!!";
+   newFacts.push = facts[i] + "!!!";
    i++;
 }
   return facts;
