d497fe7205c59515d99bdd950b2f0b096f5304e1
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-22 01:59:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts(facts) {
  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout","He hated the sound of his own voice"];
  var i = 0;
  while (i < 4) {
-   facts = facts[i] + "!!!";
+   facts[i] = facts[i] + "!!!";
    i++;
 }
   return facts;
