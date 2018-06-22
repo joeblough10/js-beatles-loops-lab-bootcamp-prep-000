3c7c745e71e7a648a2fa7c1de24559b8703868ea
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-22 01:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts("He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout","He hated the sound of his own voice") {
+function johnLennonFacts(facts) {
+ var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout","He hated the sound of his own voice"];
  var i = 0
  while (i < 4) {
    facts[i] += "!!!";
