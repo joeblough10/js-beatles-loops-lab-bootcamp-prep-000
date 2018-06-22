2d319e82426080e068ff1e5bbdea1e2e6b67a0c4
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-22 02:09:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function theBeatlesPlay(musicians, instruments) {
 "He was never a vegetarian", 
 "He was a choir boy and boy scout", 
 "He hated the sound of his own voice"]; 
-function JohnLennonFacts(arr)    { 
+function johnLennonFacts(arr)    { 
 var result= [];  
 var i = 0;  
 while(i < arr.length) { 
